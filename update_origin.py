2e9a91cf7817129b33bd777ed5bbd18f4fe16a49
DreamingLri <3507415455@qq.com>
2024-10-23 09:26:45 +0800

    ✨ change update_origin to mc version 1.21.2

--- update_origin.py
+++ update_origin.py
@@ -2,11 +2,11 @@ import requests
 import os
 
 files = {
-    'litematica': 'https://github.com/sakura-ryoko/litematica/raw/pre-rewrite/fabric/1.21/src/main/resources/assets/litematica/lang/en_us.json',
-    'malilib': 'https://github.com/sakura-ryoko/malilib/raw/pre-rewrite/fabric/1.21/src/main/resources/assets/malilib/lang/en_us.json',
-    'minihud': 'https://github.com/sakura-ryoko/minihud/raw/pre-rewrite/fabric/1.21/src/main/resources/assets/minihud/lang/en_us.json',
-    'tweakeroo': 'https://github.com/sakura-ryoko/tweakeroo/raw/pre-rewrite/fabric/1.21/src/main/resources/assets/tweakeroo/lang/en_us.json',
-    'itemscroller': 'https://github.com/sakura-ryoko/itemscroller/raw/pre-rewrite/fabric/1.21/src/main/resources/assets/itemscroller/lang/en_us.json',
+    'litematica': 'https://github.com/sakura-ryoko/litematica/raw/pre-rewrite/fabric/1.21.2/src/main/resources/assets/litematica/lang/en_us.json',
+    'malilib': 'https://github.com/sakura-ryoko/malilib/raw/pre-rewrite/fabric/1.21.2/src/main/resources/assets/malilib/lang/en_us.json',
+    'minihud': 'https://github.com/sakura-ryoko/minihud/raw/pre-rewrite/fabric/1.21.2/src/main/resources/assets/minihud/lang/en_us.json',
+    'tweakeroo': 'https://github.com/sakura-ryoko/tweakeroo/raw/pre-rewrite/fabric/1.21.2/src/main/resources/assets/tweakeroo/lang/en_us.json',
+    'itemscroller': 'https://github.com/sakura-ryoko/itemscroller/raw/pre-rewrite/fabric/1.21.2/src/main/resources/assets/itemscroller/lang/en_us.json',
     'syncmatica': 'https://github.com/sakura-ryoko/syncmatica/raw/1.21/src/main/resources/assets/syncmatica/lang/en_us.json',
     'litematica-printer': 'https://github.com/sakura-ryoko/litematica-printer/raw/1.21/src/main/resources/assets/litematica-printer/lang/en_us.json'
 }
